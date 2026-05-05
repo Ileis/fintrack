447a165ce2f1e01f5170ad2a60f933cbe141bd61
ileis <gabrielileis07@gmail.com>
2026-05-05 20:13:33 -0300

    fix exceptions in service

---
+++ back-end/demo/src/main/java/com/fintrack/demo/exception/GlobalExceptionHandler.java
@@ -0,0 +1,8 @@
+package com.fintrack.demo.exception;
+
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class GlobalExceptionHandler {
+    
+}
--- back-end/demo/src/main/java/com/fintrack/demo/service/impl/TransactionServiceImpl.java
+++ back-end/demo/src/main/java/com/fintrack/demo/service/impl/TransactionServiceImpl.java
@@ -3,13 +3,17 @@ package com.fintrack.demo.service.impl;
 import java.math.BigDecimal;
 import java.time.LocalDateTime;
 import java.util.List;
+import java.util.Locale.Category;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
 import org.springframework.web.server.ResponseStatusException;
 
+import com.fintrack.demo.exception.BusinessException;
+import com.fintrack.demo.exception.ResourceNotFoundException;
 import com.fintrack.demo.model.Item;
 import com.fintrack.demo.model.Transaction;
+import com.fintrack.demo.repository.CategoryRepository;
 import com.fintrack.demo.repository.ItemRepository;
 import com.fintrack.demo.repository.TransactionRepository;
 import com.fintrack.demo.service.TransactionService;
@@ -20,15 +24,16 @@ import lombok.RequiredArgsConstructor;
 @RequiredArgsConstructor
 public class TransactionServiceImpl implements TransactionService {
     private final TransactionRepository transactionRepository;
+    private final CategoryRepository categoryRepository;
     private final ItemRepository itemRepository;
 
     @Override
     public Transaction createTransaction(Transaction transaction) {
-        if (transactionRepository.existsById(transaction.getId()))
-            throw new ResponseStatusException(HttpStatus.BAD_REQUEST, "Transaction with the same ID already exists");
+        if (transaction.getId() != null)
+            throw new IllegalArgumentException("New transaction cannot have an ID");
 
         if (transaction.getTotalAmount().compareTo(BigDecimal.ZERO) < 0)
-            throw new ResponseStatusException(HttpStatus.BAD_REQUEST, "Total amount cannot be negative");
+            throw new IllegalArgumentException("Total amount cannot be negative");
 
         return transactionRepository.save(transaction);
     }
@@ -36,13 +41,13 @@ public class TransactionServiceImpl implements TransactionService {
     @Override
     public Transaction getTransactionById(Long id) {
         return transactionRepository.findById(id)
-                .orElseThrow(() -> new ResponseStatusException(HttpStatus.NOT_FOUND, "Transaction not found"));
+                .orElseThrow(() -> new ResourceNotFoundException("Transaction do not exists"));
     }
 
     @Override
     public List<Transaction> getTransactionsByPeriod(LocalDateTime startDate, LocalDateTime endDate) {
         if (startDate.isAfter(endDate))
-            throw new ResponseStatusException(HttpStatus.BAD_REQUEST, "Start date must be before end date");
+            throw new BusinessException("start date must be before end date");
 
         return transactionRepository.findByDateAndTimeBetween(startDate, endDate);
     }
@@ -52,7 +57,10 @@ public class TransactionServiceImpl implements TransactionService {
             LocalDateTime endDate,
             Long categoryId) {
         if (startDate.isAfter(endDate))
-            throw new ResponseStatusException(HttpStatus.BAD_REQUEST, "Start date must be before end date");
+            throw new BusinessException("start date must be before end date");
+
+        if (categoryRepository.findById(categoryId).isEmpty())
+            throw new ResourceNotFoundException("Category do not exists");
 
         return transactionRepository.findByCategoryAndDateAndTimeBetween(categoryId, startDate, endDate);
     }
