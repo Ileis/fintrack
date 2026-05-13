dc8381b297e99c520b5e262733a546cd043e9687
ileis <gabrielileis07@gmail.com>
2026-05-13 18:08:01 -0300

    feat(backend): interface do CategoryService definida

--- back-end/demo/src/main/java/com/fintrack/demo/model/dto/category/CategoryRequestDTO.java
+++ back-end/demo/src/main/java/com/fintrack/demo/model/dto/category/CategoryRequestDTO.java
@@ -2,7 +2,7 @@ package com.fintrack.demo.model.dto.category;
 
 import com.fintrack.demo.model.enums.TypeCategory;
 
-public record CategoryCreateRequestDTO(
+public record CategoryRequestDTO(
     String name,
     String color,
     String icon,
--- back-end/demo/src/main/java/com/fintrack/demo/service/CategoryService.java
+++ back-end/demo/src/main/java/com/fintrack/demo/service/CategoryService.java
@@ -3,15 +3,16 @@ package com.fintrack.demo.service;
 import java.util.List;
 
 import com.fintrack.demo.model.Category;
+import com.fintrack.demo.model.dto.category.CategoryRequestDTO;
 import com.fintrack.demo.model.enums.TypeCategory;
 
 public interface CategoryService {
-    Category createCategory(Category category);
+    Category createCategory(CategoryRequestDTO category);
 
     Category getCategoryById(Long id);
     List<Category> getCategoriesByType(TypeCategory typeCategory);
 
-    Category updateCategory(Category category);
+    Category updateCategory(Long id, CategoryRequestDTO category);
 
     void deleteCategory(Long id);
 }
